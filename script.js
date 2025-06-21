85fc1706211ed9a484609a3a09415f779552c959
NicoloTavano <nikybasket2003@gmail.com>
2025-06-21 12:51:20 +0200

    prova

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ function toggleServizio(element) {
     const content = element.nextElementSibling;
     content.style.display = content.style.display === "block" ? "none" : "block";
   }
+
+
+    const burger = document.getElementById('burger');
+  const menu = document.getElementById('menu');
+
+  burger.addEventListener('click', () => {
+    menu.classList.toggle('active');
+  });
